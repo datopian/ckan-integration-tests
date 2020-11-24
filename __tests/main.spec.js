e516b83f95502a974014bfeb1eed7e0f8c04ac9c
Thadeu Cotts <thadeu.cotts@datopian.com>
2020-11-24 16:10:45 -0300

    [tests] [s]: add example file on main test dir

---
+++ __tests/main.spec.js
@@ -0,0 +1,10 @@
+const mocha = require('mocha')
+const chai = require('chai')
+
+const expect = chai.expect
+
+describe('test', ()=> {
+  it('should run', () =>{
+    expect(true).to.be.eq(true)
+  })
+})
